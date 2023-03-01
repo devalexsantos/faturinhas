5b961cb294ba278eb219af3f3a270d32e805d80e
Alex Santos <devalexsantos@gmail.com>
2023-03-01 16:00:15 -0300

    first commit install first authenticate

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
+
+
+export function Home() {
+    const { user, killSessionFirebase } = useContext(AuthContext)
+
+    if(!user){
+        return <p>Carregando...</p>
+    }
+
+    return (
+        <div>
+            <h1>Email: {user.email}</h1>
+            <p>UID: {user.uid}</p>
+            <button>Editar</button><br/>
+            <button onClick={killSessionFirebase} >Sair</button>
+        </div>
+    )
+}
